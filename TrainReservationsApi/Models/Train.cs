dbc664850d7594390e96d57bfff1c63381626321
Ramath <ramathmanjitha@gmail.com>
2023-10-13 11:38:58 +0530

    train model updated and tran cancel function implemented

--- TrainReservationsApi/Models/Train.cs
+++ TrainReservationsApi/Models/Train.cs
@@ -1,38 +1,37 @@
-﻿
-using MongoDB.Bson;
+﻿using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
+
 namespace TrainReservationsApi.Models
 {
+    // The Train class represents information about a train.
     public class Train
-{
-    [BsonId]
-    [BsonRepresentation(BsonType.ObjectId)]
-    public string ? id { get; set; }
-    public string name { get; set; }
-    public string departureStation { get; set; }
-    public string arrivalStation { get; set; }
-    public DateTime date { get; set; }
-    public bool isActive { get; set; }
-    public bool isPublished { get; set; }
-    public string departureTime { get; set; }
-    public string arrivalTime { get; set; }
-    public List<string> availableDates { get; set; }
-    public List<TicketAvailability>? ticketsAvailability { get; set; }
-    public List<Schedule>? schedules { get; set; }
-}
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? id { get; set; }  // Unique identifier for the train
+        public string name { get; set; }  // The name of the train
+        public string departureStation { get; set; }  // Departure station
+        public string arrivalStation { get; set; }  // Arrival station
+        public bool isActive { get; set; }  // Indicates if the train is active
+        public bool isPublished { get; set; }  // Indicates if the train is published
+        public string departureTime { get; set; }  // Departure time
+        public string arrivalTime { get; set; }  // Arrival time
+        public List<string> availableDates { get; set; }  // List of available dates
+        public int firstClassTickets { get; set; }  // Number of first class tickets available
+        public int secondClassTickets { get; set; }  // Number of second class tickets available
+        public int thirdClassTickets { get; set; }  // Number of third class tickets available
+        public int? firstClassTicketsReserved { get; set; }  // Number of first class tickets reserved (nullable)
+        public int? secondClassTicketsReserved { get; set; }  // Number of second class tickets reserved (nullable)
+        public int? thirdClassTicketsReserved { get; set; }  // Number of third class tickets reserved (nullable)
+        public List<Schedule>? schedules { get; set; }  // List of schedules for the train
+    }
 
-public class TicketAvailability
-{
-    public string trainClass { get; set; }
-    public int tickets { get; set; }
-    public int reserved { get; set; }
-}
-
-public class Schedule
-{
-    public string station { get; set; }
-    public string arrivalTime { get; set; }
-    public string departureTime { get; set; }
-}
+    // The Schedule class represents the schedule of a train, including station, arrival time, and departure time.
+    public class Schedule
+    {
+        public string station { get; set; }  // Station name
+        public string arrivalTime { get; set; }  // Arrival time at the station
+        public string departureTime { get; set; }  // Departure time from the station
+    }
 
 }
--- TrainReservationsApi/Services/TrainService.cs
+++ TrainReservationsApi/Services/TrainService.cs
@@ -4,13 +4,17 @@ using TrainReservationsApi.Models;
 
 namespace TrainReservationsApi.Services
 {
+    // This service class is responsible for interacting with the MongoDB database
+    // to perform CRUD (Create, Read, Update, Delete) operations on Train objects.
+
     public class TrainService
     {
         private readonly IMongoCollection<Train> _trainsCollection;
 
-        public TrainService(
-            IOptions<TrainReservationsDatabaseSettings> TrainReservationsDatabaseSettings)
+        public TrainService(IOptions<TrainReservationsDatabaseSettings> TrainReservationsDatabaseSettings)
         {
+            // Initialize the TrainService with database connection settings.
+
             var mongoClient = new MongoClient(
                 TrainReservationsDatabaseSettings.Value.ConnectionString);
 
@@ -21,18 +25,23 @@ namespace TrainReservationsApi.Services
                 TrainReservationsDatabaseSettings.Value.TrainsCollectionName);
         }
 
+        // Retrieve all trains from the database.
         public async Task<List<Train>> GetAllAsync() =>
             await _trainsCollection.Find(_ => true).ToListAsync();
 
+        // Retrieve a specific train by its unique ID.
         public async Task<Train?> GetByIdAsync(string id) =>
             await _trainsCollection.Find(x => x.id == id).FirstOrDefaultAsync();
 
+        // Create a new train record in the database.
         public async Task CreateAsync(Train newTrain) =>
             await _trainsCollection.InsertOneAsync(newTrain);
 
+        // Update an existing train's information in the database.
         public async Task UpdateAsync(string id, Train updatedTrain) =>
             await _trainsCollection.ReplaceOneAsync(x => x.id == id, updatedTrain);
 
+        // Delete a train record from the database using its ID.
         public async Task DeleteAsync(string id) =>
             await _trainsCollection.DeleteOneAsync(x => x.id == id);
     }
